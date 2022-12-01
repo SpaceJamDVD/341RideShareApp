f3e4e46f14ff1d2c186a0870526457ea0ce018cc
JRamos2425 <jasonramosm@gmail.com>
2022-12-01 12:27:45 -0800

    Firebase Auth and Realtime Database

---
+++ app/src/main/java/com/example/a341group/Login.java
@@ -0,0 +1,100 @@
+package com.example.a341group;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+// make sure to go to SDK Manager-> SDK tools and enable google play services
+
+public class Login extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    TextView moveToRegisterBtn;
+    EditText emailInput;
+    EditText passwordInput;
+
+    Button signInBtn;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        moveToRegisterBtn = findViewById(R.id.moveToRegisterBtn);
+        emailInput = findViewById(R.id.emailInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        signInBtn = findViewById(R.id.signInBtn);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+
+        moveToRegisterBtn.setOnClickListener(v -> {
+            startActivity(new Intent(Login.this, Register.class));
+        });
+
+        signInBtn.setOnClickListener(v -> {
+            signInUser();
+        });
+
+    }
+
+    private void signInUser(){
+        String email = emailInput.getText().toString().trim();
+        String password = passwordInput.getText().toString().trim();
+
+        if (email.isEmpty()){
+            emailInput.setError("Email is required!");
+            emailInput.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailInput.setError("Please provide a valid email!");
+            emailInput.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            passwordInput.setError("Password is required!");
+            passwordInput.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6){
+            passwordInput.setError("Password should be 6 or more characters!");
+            passwordInput.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    startActivity(new Intent(Login.this, RideOrDriveActivity.class));
+                } else {
+                    Toast.makeText(Login.this, "Failed to login! Please check your credentials", Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/example/a341group/Register.java
@@ -0,0 +1,122 @@
+package com.example.a341group;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+// make sure to go to SDK Manager-> SDK tools and enable google play services
+
+public class Register extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    TextView moveToLoginBtn;
+    EditText usernameInput;
+    EditText emailInput;
+    EditText passwordInput;
+
+    Button registerBtn;
+    ProgressBar progressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        moveToLoginBtn = findViewById(R.id.moveToLoginBtn);
+        usernameInput = findViewById(R.id.usernameInput);
+        emailInput = findViewById(R.id.emailInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        registerBtn = findViewById(R.id.registerBtn);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+
+        moveToLoginBtn.setOnClickListener(v -> {
+            startActivity(new Intent(Register.this, Login.class));
+        });
+
+        registerBtn.setOnClickListener(v -> {
+            registerUser();
+        });
+    }
+
+    private void registerUser(){
+        String username = usernameInput.getText().toString().trim();
+        String email = emailInput.getText().toString().trim();
+        String password = passwordInput.getText().toString().trim();
+
+        if (username.isEmpty()){
+            usernameInput.setError("Username is required!");
+            usernameInput.requestFocus();
+            return;
+        }
+        if (email.isEmpty()){
+            emailInput.setError("Email is required!");
+            emailInput.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailInput.setError("Please provide a valid email!");
+            emailInput.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            passwordInput.setError("Password is required!");
+            passwordInput.requestFocus();
+            return;
+        }
+        if (password.length() < 6){
+            passwordInput.setError("Password should be 6 or more characters!");
+            passwordInput.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            User user = new User(username, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()){
+                                                startActivity(new Intent(Register.this, Login.class));
+                                            } else {
+                                                Toast.makeText(Register.this, "Failed to register, Try again!", Toast.LENGTH_LONG).show();
+                                                progressBar.setVisibility(View.GONE);
+                                            }
+                                        }
+                                    });
+                        } else {
+                            Toast.makeText(Register.this, "Failed to register, Try again!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+    }
+
+}
