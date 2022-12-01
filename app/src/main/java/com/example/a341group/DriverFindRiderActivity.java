713ceecf7d7fcd398ae731b61550e00f2c0c4a0a
kevin wang <kevin@kevins-MacBook-Pro-156.local>
2022-12-01 01:01:12 -0800

    yo

--- app/src/main/java/com/example/a341group/DriverFindRiderActivity.java
+++ app/src/main/java/com/example/a341group/DriverFindRiderActivity.java
@@ -5,19 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class DriverFindRiderActivity extends AppCompatActivity {
 
-    //TODO: add list of possible riders
-
+    //hard coded
+    String[] rider1;
+    String[] rider2;
+    String[] rider3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_find_rider);
+        //name, location, destination, time
+        rider1=new String[]{"Gus H", "UBCO", "Rutland", "3:30PM"};
+        rider2=new String[]{"Ramos L", "Kelowna Airport", "Rutland", "3:50PM"};
+        rider3=new String[]{"Kevin W", "UBCO", "Orchard Park Mall", "2:20PM"};
+        Button r1 = (Button)findViewById(R.id.rider1);
+        Button r2 = (Button)findViewById(R.id.rider2);
+        Button r3 = (Button)findViewById(R.id.rider3);
+        r1.setText(rider1[0]+" Wants to go to: "+rider1[1]+" \n from: "+rider1[2]+" at "+rider1[3]);
+        r2.setText(rider2[0]+" Wants to go to: "+rider2[1]+" \n from: "+rider2[2]+" at "+rider2[3]);
+        r3.setText(rider3[0]+" Wants to go to: "+rider3[1]+" \n from: "+rider3[2]+" at "+rider3[3]);
     }
 
-    public void onDriverBack(View view){
-        Intent intent = new Intent(this, RideOrDriveActivity.class);
+    public void rider1Click(View view){
+        Intent intent = new Intent(this, DriverConfirmRider.class);
+        intent.putExtra("riderInfo",rider1);
+        startActivity(intent);
+    }
+    public void rider2Click(View view){
+        Intent intent = new Intent(this, DriverConfirmRider.class);
+        intent.putExtra("riderInfo",rider2);
+        startActivity(intent);
+    }
+    public void rider3Click(View view){
+        Intent intent = new Intent(this, DriverConfirmRider.class);
+        intent.putExtra("riderInfo",rider3);
         startActivity(intent);
     }
 
