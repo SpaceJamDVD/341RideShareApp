4b3b4a5e6a4a04ece8a83bffb0a04f59eb24a7f0
JRamos2425 <jasonramosm@gmail.com>
2022-12-04 18:33:09 -0800

    Driver Views Routes

--- app/src/main/java/com/example/a341group/DriverMenu.java
+++ app/src/main/java/com/example/a341group/DriverMenu.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -70,6 +74,15 @@ public class DriverMenu extends AppCompatActivity {
 
         adapter = new PassengerCardAdapter(this, R.layout.passenger_card, passengerCards);
         myListView.setAdapter(adapter);
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String uid = passengerCards.get(i).getDocumentUID();
+                Intent intent = new Intent(DriverMenu.this, DriverViewRoute.class);
+                intent.putExtra("documentId", uid);
+                startActivity(intent);
+            }
+        });
 
         Button hereBtn = findViewById(R.id.hereBtn);
         hereBtn.setOnClickListener(v -> {
